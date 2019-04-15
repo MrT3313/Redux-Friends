f790b0fa41bf70fa72946cf0fd45c85b682d458e
MrT3313 <rturgeon13@gmail.com>
2019-04-15 16:05:41 -0500

    login styling updated & <Login /> state being updated with user inputs

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,20 +1,71 @@
 // REACT
     import React, { Component } from 'react';
 
+// MATERIAL IU
+    import { Paper, TextField, Button } from '@material-ui/core'
+// STYLED COMPONENTS
+    import styled from 'styled-components'
+
 // -- ** START CODE ** -- //
 // -- ** START CODE ** -- //
 
+// STYLING -- START (combination of styled-components & material-ui)
+    const Styled_Paper = styled(Paper)`
+        display: flex;
+        flex-direction: column;
+
+        width: 50%;
+
+        padding: 20px;
+        margin: 50px auto 0 auto;
+        
+        h2 {
+            text-align: center;
+        }
+    `;
+    const Styled_TextField = styled(TextField)`
+
+    `;
+    const Styled_Button = styled(Button)`
+        
+    `;
+// STYLING -- END
 export default class Login extends Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input_userName: '',
+            input_password: ''
+        }
+    }
 
-    //     }
-    // }
+    hander_onChange = e => {
+        this.setState( 
+            { [e.target.id] : e.target.value }
+        )
+    }
 
     render() {
         return(
-            <h2>LOGIN</h2>        
+            <>
+                <Styled_Paper>
+                    <h2>Please Login</h2>
+                    <Styled_TextField
+                        id="input_userName"
+                        label="USERNAME"
+                        onChange={this.hander_onChange}
+                    />
+                    <Styled_TextField
+                        id="input_password"
+                        label="PASSWORD"
+                        onChange={this.hander_onChange}
+                    />
+                    <Styled_Button 
+                        variant="contained" 
+                        
+                    >Submit</Styled_Button>
+                </Styled_Paper>
+            </>
         )
     }
 }
